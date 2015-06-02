2d0f00d9f2844e903e4bff0033f665ac90608eca
unknown <vladymir.ovcharov@zt-082.ldlproject.local>
2015-06-02 18:09:55 +0300

    Initial commit

---
+++ src/org/titan/javaHome/Main.java
@@ -0,0 +1,116 @@
+package org.titan.javaHome;
+
+import java.util.Random;
+
+/**
+ * Created by vladymir.ovcharov on 02/06/2015.
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 15;
+        int[] mas = new int[n];
+        int[] mas1 = new int[n];
+        Random rand = new Random();
+        System.out.println("========================Find max element=================================");
+//  Generating a mas
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = rand.nextInt(20);
+        }
+
+//  Copying a mas in mas1
+        for (int i = 0; i < mas.length; i++) {
+            mas1[i] = mas[i];
+        }
+
+//  Output generated mass
+        System.out.print("Generated mass: ");
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print("[" + mas[i] + "]");
+        }
+        System.out.println();
+
+//  Sotring a mas
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas.length - 1; j++) {
+                if (mas[j] > mas[j + 1]) {
+                    int temp = mas[j];
+                    mas[j] = mas[j + 1];
+                    mas[j + 1] = temp;
+                }
+            }
+        }
+//  Output sorted mas
+        System.out.println();
+        System.out.print("Sorted mass: ");
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print("[" + mas[i] + "]");
+        }
+        System.out.println();
+
+//  Looking for a max number
+        System.out.println();
+
+        int max1 = mas1[0];
+        for (int i = 0; i < mas.length; i++) {
+            if(max1 > mas[i]){
+                continue;
+            } else max1 = mas[i];
+        }
+
+//  Output max
+        System.out.println("Max element in mas(sorted) is: " + mas[mas.length-1]);
+        System.out.println("Max element in mas1(copied) is: " + max1);
+        System.out.println();
+
+        System.out.println("========================Arithmetic mean==================================");
+        int sum = 0;
+        for (int i = 0; i < mas1.length; i++) {
+            sum = sum + mas1[i];
+        }
+        int arithmeticMean = sum/mas.length;
+        System.out.println("Arithmetic mean is: " + arithmeticMean);
+        System.out.println();
+
+        System.out.println("===================The closest number to arithmetic mean=================");
+
+        int difference = 99999;  //rly big number which will be never used
+//  Looking for minimal difference
+        for (int i = 0; i < mas1.length; i++) {
+            if(Math.abs(arithmeticMean - mas1[i]) < difference){
+                difference = Math.abs(arithmeticMean - mas1[i]);
+            }
+        }
+//  Create masOfDifferences
+        int[] masOfDifferences = new int[n];
+        for (int i = 0; i < mas1.length; i++) {
+            masOfDifferences[i] = Math.abs(arithmeticMean - mas1[i]);
+        }
+//  Output all the symbols, wich are equal to minimal
+        System.out.print("The closest numbers to arithmetic mean are: ");
+        for (int i = 0; i < masOfDifferences.length; i++) {
+            if(masOfDifferences[i] == difference){
+                System.out.print(mas1[i] + " ");
+            }
+        }
+        System.out.println();
+        System.out.println();
+
+        System.out.println("======================Output all equal numbers===========================");
+
+        int equal;
+        for (int i = 0; i < mas1.length; i++) {
+
+        }
+
+
+
+
+
+
+
+    }
+}
+
+
